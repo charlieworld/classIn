8cf9c83990e63a3daca4d70ebfa28308f9ab8eba
Charlieeeee <charlieworld2008@gmail.com>
2018-09-17 15:07:11 +0800

    [version] 1.2.2

--- asset/app.js
+++ asset/app.js
@@ -35,7 +35,6 @@ new Vue({
   created: function () {
     let path = window.location.pathname;
     path = path.split('/')[1];
-    console.log('path :',path);
     if (path === 'view')
     {
       this.parseURLSearch();
