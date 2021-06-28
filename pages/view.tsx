d72843586677b7f6facc84b226a6491ad5f3eb93
charlieworld <charlieworld2008@gmail.com>
2021-06-28 10:33:29 +0800

    [Refactor] add view page empty component

---
+++ pages/view.tsx
@@ -0,0 +1,16 @@
+import MainLayout from '../components/MainLayout';
+import Heart from '../components/Icon/Heart';
+import Sparkles from '../components/Icon/Sparkles';
+import Clock from '../components/Icon/Clock';
+export default function Index() {
+  const S_MARGIN = 'mb-6';
+  const ICON_MARGIN = 'mb-1';
+
+  return (
+    <MainLayout >
+      <main className="w-full h-screen py-24 px-10">
+        view
+      </main>
+    </MainLayout>
+  );
+}
